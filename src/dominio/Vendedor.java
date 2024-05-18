258dc1aa1682afb02a5d5247ea3b8695756d4cc8
Gabriel Santos Lemos <gabrielsantoslemos.com@gmail.com>
2024-05-17 21:51:03 -0300

    Criar classe Vendedor.

---
+++ src/dominio/Vendedor.java
@@ -0,0 +1,22 @@
+package dominio;
+
+public class Vendedor {
+    private Number id;
+    private String nome;
+
+    public Vendedor(String nome) {
+        this.nome = nome;
+    }
+
+    public Number id() {
+        return this.id;
+    }
+
+    public String nome() {
+        return this.nome;
+    }
+
+    public void nome(String value) {
+        this.nome = value;
+    }
+}
